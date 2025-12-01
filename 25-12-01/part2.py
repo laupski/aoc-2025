528aeb206030e4732b05f9b6fa67f08a74ba0e3c
Paul Kawalkowski <pkawalkowski@gmail.com>
2025-12-01 01:08:56 -0500

    feat: 1st day

---
+++ 25-12-01/part2.py
@@ -0,0 +1,29 @@
+def solve_safe_puzzle(filename):
+    with open(filename, 'r') as f:
+        rotations = [line.strip() for line in f if line.strip()]
+
+    position = 50
+    zero_count = 0
+
+    for rotation in rotations:
+        direction = rotation[0]
+        distance = int(rotation[1:])
+
+        for _ in range(distance):
+            if direction == 'L':
+                position = (position - 1) % 100
+            else: 
+                position = (position + 1) % 100
+            
+            if position == 0:
+                zero_count += 1
+
+    return zero_count
+
+
+def main():
+    actual_result = solve_safe_puzzle('input.txt')
+    print(actual_result)
+
+if __name__ == "__main__":
+    main()
