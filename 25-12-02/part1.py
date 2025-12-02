98ab6646408a46ddb0582bfcf8f61985cbb6c7de
Paul Kawalkowski <pkawalkowski@gmail.com>
2025-12-02 12:52:56 -0500

    feat: day 2

---
+++ 25-12-02/part1.py
@@ -0,0 +1,42 @@
+def solve_id_puzzle(filename):
+    with open(filename, "r") as f:
+        ids = f.read().strip()
+
+    sum = 0
+
+    for id in ids.split(","):
+        start, end = id.split("-")
+        start = int(start)
+        end = int(end)
+        for i in range(start, end):
+            if isRepeat(i):
+                print(i)
+                sum += i
+
+    return sum
+
+
+def isRepeat(num: int) -> bool:
+    snum = str(num)
+    length = len(snum)
+
+    if length < 2 or length % 2 != 0:
+        return False
+
+    half = length // 2
+    first_half = snum[:half]
+    second_half = snum[half:]
+
+    if first_half == second_half:
+        return True
+
+    return False
+
+
+def main():
+    sum = solve_id_puzzle("input.txt")
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
---
+++ 25-12-02/part2.py
@@ -0,0 +1,42 @@
+def solve_id_puzzle(filename):
+    with open(filename, "r") as f:
+        ids = f.read().strip()
+
+    sum = 0
+
+    for id in ids.split(","):
+        start, end = id.split("-")
+        start = int(start)
+        end = int(end)
+        for i in range(start, end):
+            if isRepeat(i):
+                print(i)
+                sum += i
+
+    return sum
+
+
+def isRepeat(num: int) -> bool:
+    snum = str(num)
+    length = len(snum)
+
+    for pattern_len in range(1, length // 2 + 1):
+        if length % pattern_len != 0:
+            continue
+
+        pattern = snum[:pattern_len]
+        repetitions = length // pattern_len
+
+        if pattern * repetitions == snum:
+            return True
+
+    return False
+
+
+def main():
+    sum = solve_id_puzzle("input.txt")
+    print(sum)
+
+
+if __name__ == "__main__":
+    main()
